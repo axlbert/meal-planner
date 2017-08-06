b7c2d1e4998346c2fbff554676997ac265ea3cd2
axlbert <aclanger@hotmail.de>
2017-08-06 20:37:33 +0200

    app completed

---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+const API_ID = "1234"
+const APP_KEY = "34451d2dfd3"
+
+
+export function fetchRecipes (food = '') {
+  food = food.trim()
+
+  return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
+    .then((res) => res.json())
+    .then(({ hits }) => hits.map(({ recipe }) => recipe))
+}
